cafc694accd11eb5cc036fbc46b0f5cecad3c3c0
Jefferson Fonseca <drpcscan@gmail.com>
2021-07-28 17:09:18 -0300

    d

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const mongo = ()=>{
-  return  mongoose.connect("mongodb+srv://alertaassaltoDB:g6VnL150SNQ7248j@db-mongodb-nyc3-03622-6120d5d2.mongo.ondigitalocean.com/AlertaAssaltoDB?authSource=admin&replicaSet=db-mongodb-nyc3-03622&tls=true", {sslValidate: false, ssl: true,useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true})
+  return  mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/${process.env.DB_DATABASE}?authSource=admin&replicaSet=${process.env.DB_REPLICASET}&tls=true`, {sslValidate: false, ssl: true,useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true})
 }
 
 const connect = ()=>{
